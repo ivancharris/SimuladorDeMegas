26192ed4557d6a80bac806ee1e561a94a6283c08
Ivan Charris <ivancharrisc@gmail.com>
2021-07-21 22:53:53 -0600

    actualizacion

--- Simulación 11Jul/src/cr/ac/ulead/main/simulador1.java
+++ Simulación 11Jul/src/cr/ac/ulead/main/simulador1.java
@@ -9,47 +9,40 @@ public class simulador1 {
 
 	public static void main(String[] args) {
 
-		int dias, capIni;
+		int dias, capIni = 0;
 		int capDia = 7000;
-		double capExt = 0;
-		double acumulador,sobrante = 0,capSig = 0;
+		double sobrante = 10000, acumulador=0;
 
 		
-		output.println("Ingrese la cantidad inicial (día 0) ");
-		capIni= input.nextInt();
+		output.println("Ingrese la cantidad de megas iniciales (día 0) ");
+		sobrante= input.nextInt();
 		output.println("Ingrese los días que desea simular ");
 		dias= input.nextInt();
-		
-		if (capIni <= 28000) {
-			capExt = capIni * 1.4;
-		} else {
-			capExt = capIni * 1.31;
-		}
-		
 
-		output.println("Capacidad megas iniciales " + capIni);
+		output.println("Capacidad megas iniciales " + sobrante);
 		output.println("Capacidad de consumo diario por usuarios " + capDia);
-		output.println("Capacidad diaria agregada " + capExt);
 		output.println(" ");
 		
-		for (int i = 1; i <= dias; i++) {
-			sobrante = capExt - capDia;
-			
+		for (int i = 0; i <= dias; i++) {
+			capIni=(int) sobrante;
 			
 			if (sobrante <= 28000) {
-				capExt = sobrante * 1.4;
+				sobrante = sobrante * 1.4;
 			} else {
-				capExt = sobrante * 1.31;
+				sobrante = sobrante * 1.31;
 			}
+			acumulador=sobrante;
+			sobrante = acumulador - capDia;
 			
-			output.println("Día " + i);
-			output.println("Capacidad diaria agregada " + Math.round(capExt));
+			
+			output.println("Día " + (i));
+			output.println("Capacidad de megas iniciales " + Math.round(capIni));
+			output.println("Capacidad diaria agregada " + Math.round(acumulador));
 			output.println("Capacidad para el día siguiente " + Math.round(sobrante));
 			output.println(" ");
 		}
 		
-
-		output.println("Memoría disponible para el día " + (dias+1)  +" es "+ sobrante);
+		output.println("Memoría disponible para el día " + (dias+1)  +" es "+ Math.round(sobrante));
 
 	}
 }
